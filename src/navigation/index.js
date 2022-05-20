1bee1dbf1639fe43dd663a468081299f1974d455
jules <thomas.desessarts-jules@hotmail.com>
2022-05-20 15:41:54 +0200

    first commit with good project config

---
+++ src/navigation/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Landing from '../features/onboarding/landing/landing.screen'
+import Landingfct from '../features/onboarding/landingfct/landing.screen'
+import Login from '../features/onboarding/login/login.screen'
+
+const Stack = createNativeStackNavigator();
+
+const rootContainer = () => {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator
+							screenOptions={{
+								headerShown: false
+							}}
+						>
+							<Stack.Screen
+								name="landing"
+								component={Landing}
+							/>
+							<Stack.Screen
+								name="landingfct"
+								component={Landingfct}
+							/>
+							<Stack.Screen
+								name="login"
+								component={Login}
+							/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+export default rootContainer;
